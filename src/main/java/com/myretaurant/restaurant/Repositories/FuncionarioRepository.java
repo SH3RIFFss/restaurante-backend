4b59e033c9085b6d66b2efdedded35a2fc326526
sheriff <picadetitani1@gmail.com>
2026-01-19 01:24:38 -0400

    configuração do repositories

--- src/main/java/com/myretaurant/restaurant/Repositories/FuncionarioRepository.java
+++ src/main/java/com/myretaurant/restaurant/Repositories/FuncionarioRepository.java
@@ -3,7 +3,7 @@ package com.myretaurant.restaurant.Repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.myretaurant.restaurant.Models.FuncionarioModel;
-
+//faz a lincagem da chave primaria para fora da tabela
 public interface FuncionarioRepository extends JpaRepository<FuncionarioModel, Integer>{
 
 }
