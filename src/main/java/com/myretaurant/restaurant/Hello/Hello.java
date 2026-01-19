b24f2dc6727d7b0be51808a1a7269d5d0d53b910
sheriff <picadetitani1@gmail.com>
2026-01-19 11:24:41 -0400

    comentario de descrição em um metodo

--- src/main/java/com/myretaurant/restaurant/Hello/Hello.java
+++ src/main/java/com/myretaurant/restaurant/Hello/Hello.java
@@ -1,5 +1,5 @@
 package com.myretaurant.restaurant.Hello;
-
+//texto para indicar a finalização da etapa de inicialização.
 public class Hello {
     public void hello(String string){
         System.out.println("\nola meu criador :"+string+". Sua aplicação esta a pleno vapor....\n");
